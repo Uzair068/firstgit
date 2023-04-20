3c4a385c9e13dd3bc1d346e6bb46c35d2431cc05
Mohammed uzair mauzam <uzairmauzam68@gmail.com>
2023-04-20 15:14:40 +0530

    B-green hidden

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // GETELEMENTSBYCLASSNAME //
 var items = document.getElementsByClassName('list-group-item');
-items [2].style.backgroundColor = 'green';
+items [1].style.backgroundColor = 'green';
+items [2].style.display = 'none';
 
 var allitems=document.getElementById('items')
 allitems.style.fontWeight="bold"
@@ -18,14 +19,14 @@ allitems.style.fontWeight="bold"
 //   items[i].style.backgroundColor = '#f4f4f4';
 // }
 
-var li = document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[1].textContent = 'Hello 2';
-li[1].style.fontweight = 'bold';
-li[1].style.backgroundColor = 'yellow';
-// Gives error
-//items.style.backgroundColor = '#f4f4f4';
-for(var i = 0; i < li.length; i++){
-  li[i].style.backgroundColor = '#f4f4f4';
-}
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = 'Hello 2';
+// li[1].style.fontweight = 'bold';
+// li[1].style.backgroundColor = 'yellow';
+// // Gives error
+// //items.style.backgroundColor = '#f4f4f4';
+// for(var i = 0; i < li.length; i++){
+//   li[i].style.backgroundColor = '#f4f4f4';
+// }
