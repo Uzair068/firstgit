b4a6db56fe0cf0a9a08611eb6da583371f834670
Mohammed uzair mauzam <uzairmauzam68@gmail.com>
2023-04-20 14:37:25 +0530

    li added

--- app.js
+++ app.js
@@ -4,3 +4,28 @@ items [2].style.backgroundColor = 'green';
 
 var allitems=document.getElementById('items')
 allitems.style.fontWeight="bold"
+
+//className
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items [1]);
+// items [1].textContent = 'Hello 2';
+// items [1].style.fontweight = 'bold';
+// items [1].style.backgroundColor = 'yellow';
+// // Gives error
+// //items.style.backgroundColor = '#f4f4f4';
+// for(var i = 0; i < items.length; i++){
+//   items[i].style.backgroundColor = '#f4f4f4';
+// }
+
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+li[1].style.fontweight = 'bold';
+li[1].style.backgroundColor = 'yellow';
+// Gives error
+//items.style.backgroundColor = '#f4f4f4';
+for(var i = 0; i < li.length; i++){
+  li[i].style.backgroundColor = '#f4f4f4';
+}
