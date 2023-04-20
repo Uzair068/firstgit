a4c2fb9eb29fceb4460696aa41aa7a94303432fb
Mohammed uzair mauzam <uzairmauzam68@gmail.com>
2023-04-20 14:22:08 +0530

    Green G-g And Bold font

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('I love git')
+// GETELEMENTSBYCLASSNAME //
+var items = document.getElementsByClassName('list-group-item');
+items [2].style.backgroundColor = 'green';
+
+var allitems=document.getElementById('items')
+allitems.style.fontWeight="bold"
