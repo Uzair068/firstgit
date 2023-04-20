3fc71b51324a8b55551dcabecb9ac3ec0220a6d9
Mohammed uzair mauzam <uzairmauzam68@gmail.com>
2023-04-20 15:24:09 +0530

    color changed

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-// GETELEMENTSBYCLASSNAME //
-var items = document.getElementsByClassName('list-group-item');
-items [1].style.backgroundColor = 'green';
-items [2].style.display = 'none';
+// // GETELEMENTSBYCLASSNAME //
+// var items = document.getElementsByClassName('list-group-item');
+// items [1].style.backgroundColor = 'green';
+// items [2].style.display = 'none';
 
-var allitems=document.getElementById('items')
-allitems.style.fontWeight="bold"
+// var allitems=document.getElementById('items')
+// allitems.style.fontWeight="bold"
 
 //className
 // var items = document.getElementsByClassName('list-group-item');
@@ -29,4 +29,25 @@ allitems.style.fontWeight="bold"
 // //items.style.backgroundColor = '#f4f4f4';
 // for(var i = 0; i < li.length; i++){
 //   li[i].style.backgroundColor = '#f4f4f4';
+// }
+
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+
+// var lastItem = document.querySelector('.list-group-item:nth-child');
+// lastItem.style.color = 'blue';
+var sItem = document.querySelector('.list-group-item:nth-child(3)');
+sItem.style.color = 'blue';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color = 'coral';
+
+// var titles = document.querySelectorAll('.title');
+// console.log(titles);
+// titles[0].textContent = 'Hello';
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// var even= document.querySelectorAll('li:nth-child(even)');
+// for(var i = 0; i < odd.length; i++){
+//   odd[i].style.backgroundColor = '#f4f4f4';
+//   even[i].style.backgroundColor ='green';
 // }
